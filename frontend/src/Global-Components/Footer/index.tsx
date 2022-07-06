ca15b7b5e2be181a12f7550760988c13a24acc97
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-07-06 19:50:44 -0300

    Iniciando o Footer

---
+++ frontend/src/Global-Components/Footer/index.tsx
@@ -0,0 +1,18 @@
+export default function Footer(){
+    return(
+        <form>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <h5>Lacrei</h5>
+                <a ></a>
+                <a ></a>
+                <a ></a>
+            </div>
+            <div>
+                
+            </div>
+        </form>
+    )
+}
