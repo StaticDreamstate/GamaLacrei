9debcec5d6c9ee351f28b7156947ece0d3e2f243
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-07-07 19:39:32 -0300

    Footer finalizado

--- frontend/src/Global-Components/Footer/index.tsx
+++ frontend/src/Global-Components/Footer/index.tsx
@@ -1,40 +1,69 @@
-import { recursosArray } from "../../assets/icons"
-import { SectionStyled, DivStyled } from "./styled"
+
+import { SectionStyled, DivStyled, DivImgStyled, LastSection } from "./styled"
 import Logo from "../../assets/images/lacrei.png"
+import Face from "../../assets/icons/Face.png"
+import Email from "../../assets/icons/Email.png"
+import Instagram from "../../assets/icons/Instagram.png"
+import Linkedin from "../../assets/icons/Linkedin.png"
+
 
 export default function Footer(){
     return(
-        <SectionStyled>
-            <div>
-                <img src={Logo} alt="" />
-            </div>
-            <DivStyled>
-                <h5>Lacrei</h5>
-                <a>Início</a>
-                <a>Quem somos</a>
-                <a>Acessibilidade</a>
-            </DivStyled>
-            <DivStyled>
-                <h5>Saúde</h5>
-                <a>Lacrei Pessoas Usuárias</a>
-                <a>Lacrei Profissional da Saúde</a>
-            </DivStyled>
-            <DivStyled>
-                <h5 className="h5">Segurança e privacidade</h5>
-                <a>Portal de segurança e privacidade</a>
-                <a>Política de privacidade</a>
-                <a>Termo de uso</a>
-                <a>Direitos de Titular</a>
-            </DivStyled>
-            <div>
-                <h5>Redes sociais</h5>
-                {recursosArray.map((element,index)=>(
-                <div key={element.text+index}>
-                    <img src={element.img} alt={element.text} />
-                    <p>{element.text}</p>
+        <div>
+            <SectionStyled>
+                <div>
+                    <img src={Logo} alt="" />
                 </div>
-                ))}
-            </div>
-        </SectionStyled>
+                <DivStyled>
+                    <h5 className="h5">Lacrei</h5>
+                    <a>Início</a>
+                    <a>Quem somos</a>
+                    <a>Acessibilidade</a>
+                </DivStyled>
+                <DivStyled>
+                    <h5 className="h5">Saúde</h5>
+                    <a>Lacrei Pessoas Usuárias</a>
+                    <a>Lacrei Profissional da Saúde</a>
+                </DivStyled>
+                <DivStyled>
+                    <h5 className="h5">Segurança e privacidade</h5>
+                    <a>Portal de segurança e privacidade</a>
+                    <a>Política de privacidade</a>
+                    <a>Termo de uso</a>
+                    <a>Direitos de Titular</a>
+                </DivStyled>
+                <DivStyled>
+                    <h5 className="h5">Redes sociais</h5>
+                    <DivImgStyled>
+                        <a href="https://www.facebook.com/lacrei.saude"><img src={Face} alt="" /></a>
+                        <p>Facebook</p>
+                    </DivImgStyled>
+                    <DivImgStyled>
+                        <a href="https://www.instagram.com/explore/tags/portallacrei/"><img src={Instagram} alt="" /></a>
+                        <p>Instagram</p>
+                    </DivImgStyled>
+                    <DivImgStyled>
+                        <a href="https://www.linkedin.com/company/lacrei/"><img src={Linkedin} alt="" /></a>
+                        <p>Linkedin</p>
+                    </DivImgStyled>
+                    <DivImgStyled>
+                        <a href="contato@portallacrei.com.br"><img src={Email} alt="" /></a>
+                        <p>Email</p>
+                    </DivImgStyled>
+                    
+                </DivStyled>
+            </SectionStyled>
+            <LastSection>
+                <div>
+                    <hr />
+                </div>
+                <div>
+                    <p>A Lacrei não oferece tratamento médico emergencial. Em caso de emergência procure o hospital mais próximo.</p>
+                    <p>Em caso de problemas psicológicos, ligue para 188 (CVV) ou acesse o site www.cvv.org.br</p>
+                    <p>Copyright © 2021 Lacrei. Todos os direitos reservados. CNPJ: 35.328.975/0001-28</p>
+                </div>
+            </LastSection>
+        
+        </div>
     )
 }
--- frontend/src/Global-Components/Footer/styled.ts
+++ frontend/src/Global-Components/Footer/styled.ts
@@ -3,11 +3,12 @@ import styled from "styled-components"
 export const SectionStyled = styled.section`
     display: flex;
     flex-wrap: wrap;
+    padding: 0px 80px;
+    gap: 40px;
     div img{
         width: 48px;
         height: 48px;
-        left: 128px;
-        top: 56px;
+        margin-top: 56px;
     }
 
 `
@@ -15,6 +16,9 @@ export const SectionStyled = styled.section`
 export const DivStyled= styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: left;
+    padding: 30px;
+
 
     .h5{
         font-family: 'Nunito';
@@ -30,5 +34,37 @@ export const DivStyled= styled.div`
         font-weight: 400;
         font-size: 16px;
         line-height: 150%;
+    }
+`
+export const DivImgStyled = styled.div`
+    
+    display: flex;
+    
+    img{
+        width: 12px;
+        height: 20px;
+    }
+
+    p{
+        display: flex;
+        align-items: center;
+        padding-left: 26px;
+    }
+`
+export const LastSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    div hr{
+        width: 1110px;
+    }
+    div p{
+        font-family: 'Nunito';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 150%;
+        color: #515151;
     }
 `
