9ef3e1cc6470ac5aa94cfcceb2c05b33ae7a463e
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-07-06 22:49:11 -0300

    criação pg Pos Cad Ident  ajuste pg dados pessoais

--- frontend/src/components/Cadastro/Dados-pessoais/Cadastro-dados-pessoais.tsx
+++ frontend/src/components/Cadastro/Dados-pessoais/Cadastro-dados-pessoais.tsx
@@ -1,11 +1,24 @@
 import { HeaderStyled } from "../../../Global-Components/Header/header.style";
-import { DivDados } from "./Cadastro-dados-pessoais.style";
+import { DivDados } from "./cadastro-dados-pessoais.style";
+import avatar from "../../../assets/images/avatar.png";
 
 export default function DadosPessoais() {
   return (
-    <>
+    <div>
       <DivDados>
+        <div className="field">
+          <h2>Dados Pessoais</h2>
+        </div>
+
         <form action="#">
+          <div className="imagem">
+            <label htmlFor="imagem">Foto para perfil *</label>
+            <div className="botao1">
+              <img src={avatar} alt="user-avatar" id="imagem"></img>
+              <button>Adicionar foto</button>
+            </div>
+          </div>
+
           <div className="field">
             <label htmlFor="descricao">
               Adicione uma descrição para sua foto
@@ -24,8 +37,11 @@ export default function DadosPessoais() {
               placeholder="Fale sobre você"
             ></textarea>
           </div>
+          <div className="botao2">
+            <button type="submit">Continuar</button>
+          </div>
         </form>
       </DivDados>
-    </>
+    </div>
   );
 }
