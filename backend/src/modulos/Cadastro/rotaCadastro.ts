d19e63e80f4a71a64f438e5b1e5b351060d98f92
Pablo Cariello <pgacariello@gmail.com>
2022-07-06 21:40:29 -0300

    Att rotas cadastro e rotas pre cadastro

--- backend/src/modulos/Cadastro/rotaCadastro.ts
+++ backend/src/modulos/Cadastro/rotaCadastro.ts
@@ -4,5 +4,9 @@ import controlador from "./controlador";
 const rotaCadastro = Router();
 
 rotaCadastro.post("/cadastro", controlador.cadastro);
+rotaCadastro.post("/cadastro/dados-pessoais", controlador.cadastro);
+rotaCadastro.post("/cadastro/dados-profissionais", controlador.cadastro);
+rotaCadastro.post("/cadastro/consultorio", controlador.cadastro);
+rotaCadastro.post("/cadastro/serviços", controlador.cadastro);
 
 export default rotaCadastro;
